0fd4f5437aecd5f41949c734dab6530408645b15
Ilia Khondozhko <101108311+codiliateur@users.noreply.github.com>
2023-08-19 14:06:57 +0300

    Update gen_uuid.sql

--- custom-uuid/functions/gen_uuid.sql
+++ custom-uuid/functions/gen_uuid.sql
@@ -1,42 +1,39 @@
-CREATE OR REPLACE FUNCTION public.gen_uuid (
-  p_objtype integer = 0,
-  p_apptype integer = 0
+CREATE OR REPLACE FUNCTION gen_uuid (
+  p_entity integer = 0,
+  p_app integer = 0
 )
 RETURNS uuid AS
 $body$
--- 3.0 revision
+-- 4.0 revision
 DECLARE
-    v_ts			timestamptz DEFAULT clock_timestamp();
-    v_ms			integer 	DEFAULT mod(EXTRACT(MICROSECONDS FROM now())::integer, 1000000);
-    v_uuid          varchar(32) DEFAULT '';
-	v_objtype		varchar(4)  DEFAULT '0000';
-	v_apptype		varchar(2)  DEFAULT '00';
+    v_ts timestamptz DEFAULT clock_timestamp();
+    v_ms integer DEFAULT mod(EXTRACT(MICROSECONDS FROM now())::integer, 1000000);
+    v_uuid varchar(32) DEFAULT '';
+    v_entity varchar(4) DEFAULT '0000';
+    v_app varchar(3) DEFAULT '000';
 BEGIN
-	-- Prepare Object type value
-    IF (p_objtype > 0) THEN
-    	v_objtype := lpad(right(to_hex(p_objtype),4),4,'0');
+    -- Prepare Object type value
+    IF (p_entity > 0) THEN
+        v_entity := lpad(right(to_hex(p_entity),4),4,'0');
     END IF;
 
     -- Prepare Application type value
-    IF (p_apptype > 0) THEN
-    	v_apptype := lpad(right(to_hex(p_apptype),2),2,'0');
+    IF (p_app > 0) THEN
+    	v_app := lpad(right(to_hex(p_app),3),3,'0');
     END IF;
 
-    -- Compile UUID in format TTTTTTTT-SSSS-S0AA-OOOO-RRRRRRRRRRRR
-	---- T - Timestamp segment (8 hex-digits)
-    ---- S - Microseconds with .000000 precission
-    ---- 0 - Reserve (1 hex-digit)
-    ---- A - Application type segment	(2 hex-digits)
-	---- O - Object type segment (4 hex-digits)
+    -- Compile UUID in format TTTTTTTT-TTTT-TAAA-EEEE-RRRRRRRRRRRR
+    ---- T - Timestamp (microsecond precision) segment (13 hex-digits)
+    ---- A - Application type segment (3 hex-digits)
+    ---- E - Object type segment (4 hex-digits)
     ---- R - Random segment (12 hex-digits)
-	v_uuid := v_uuid || to_hex(EXTRACT(EPOCH FROM v_ts)::integer)::text
-    				 || lpad(to_hex(v_ms)::text,5,'0')
-                     || '0'
-                     || v_apptype
-					 || v_objtype
-     				 || lpad(to_hex((random()*65535)::bigint),4,'0')
-     				 || lpad(to_hex((random()*65535)::bigint),4,'0')
-     				 || lpad(to_hex((random()*65535)::bigint),4,'0');
+    v_uuid := v_uuid || to_hex(EXTRACT(EPOCH FROM v_ts)::integer)::text
+                     || lpad(to_hex(v_ms)::text,5,'0')
+                     || v_app
+                     || v_entity
+                     || lpad(to_hex((random()*65535)::bigint),4,'0')
+                     || lpad(to_hex((random()*65535)::bigint),4,'0')
+                     || lpad(to_hex((random()*65535)::bigint),4,'0');
 RETURN v_uuid::uuid;
 END;
 $body$
